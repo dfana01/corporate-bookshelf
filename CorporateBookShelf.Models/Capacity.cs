41605bdbce0d87f819415189dbef0bfe94f02355
Dante Faña <dfana@dfb.com.do>
2017-12-02 10:50:34 -0400

    Add capacity class and rules test

---
+++ CorporateBookShelf.Models/Capacity.cs
@@ -0,0 +1,8 @@
+﻿namespace CorporateBookShelf.Models
+{
+    public class Capacity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CorporateBookshelf.Test/CapacityRuleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using CorporateBookShelf.Models;
+
+namespace CorporateBookshelf.Test
+{
+    [TestFixture()]
+    public class CapacityRuleTest
+    {
+        [Test]
+        public void NotEmpty()
+        {
+            Capacity capacity = new Capacity()
+            {
+                Id=1,
+                Name=""
+            };
+        }
+    }
+}
