6b6696c8d67aca89024b334f911509ca03f0cb28
Eddy Leon <eddyleon21@gmail.com>
2017-12-02 11:13:44 -0400

    capacity handle

---
+++ CorporateBookshelf.App/AddCapacityHandle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorporateBookshelf.Core;
+using CorporateBookshelf.Data;
+using CorporateBookShelf.Models;
+
+namespace CorporateBookshelf.App
+{
+    public class AddCapacityHandle : IOptionHandle
+    {
+        bool IOptionHandle.Execute()
+        {
+            var book = new Capacity();
+            Console.WriteLine("Ingrese los datos de la capacidad:");
+            Console.Write("ID: ");
+           
+            try
+            {
+               ///
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine();
+                Console.Error.WriteLine($"\t{ex.Message}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            return true;
+        }
+
+        private string GetCurrentPath()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "db.json");
+        }
+    }
+}
